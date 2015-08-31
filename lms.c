e6e9ad5724f8f6aa81a1274ba78e67e7123f68b0
Samarth Kapoor <samarth003@e.ntu.edu.sg>
2015-08-31 23:02:01 +0800

    password asterisk

--- lms.c
+++ lms.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include<conio.h>
 
 struct user{
 	char uname[30];
 	char pwd[4];
 }details;
 void login(struct user details){
+	int counter;
 	printf("user name: ");
 	scanf("%s", details.uname);
 	printf("\npassword: ");
-	scanf("%s", details.pwd);
+	//scanf("%s", details.pwd);
+	for(counter=0; counter<sizeof(details.pwd); counter++){
+		details.pwd[counter] = getch();
+		printf("*");		
+	}
 }
 int main(){
 	struct user user_record;
