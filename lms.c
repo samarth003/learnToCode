d8d0337a76cd1f69fafc22ee5a6ef578eae7b280
Samarth Kapoor <samarth003@e.ntu.edu.sg>
2015-09-02 13:25:48 +0800

    setup call repetition

--- lms.c
+++ lms.c
@@ -11,36 +11,12 @@ void app_out(struct user details);
 
 int main(){
 	struct user user_record;
-	int num_select;
 	log_screen(user_record);
-	printf("\nPlease key in the number corresponding to what you want to do\n1.View your leave\n2.Apply for leave\n3.Update leave record\n4.Exit\n");
-	scanf("%d", num_select);
-	//app_out(user_record);
-	
-	switch(num_select){
-		case 1:
-			printf("testing case1.. ");
-			//view_leave();
-			break;
-		case 2: 
-			printf("testing case2.. ");
-			//apply_leave();
-			break;
-		case 3:
-			printf("testing case3.. ");
-			//update_leave();
-			break;
-		case 4:
-			app_out(user_record);
-			break;
-		default:
-			printf("Please try again!");
-			break;
-	}	
 	return 0;
 }
 void log_screen(struct user details){
 	int counter;
+	int num_select;
 	printf("user name: ");
 	scanf("%s", details.uname);
 	printf("\npassword: ");
@@ -49,7 +25,29 @@ void log_screen(struct user details){
 		details.pwd[counter] = getch();
 		printf("*");		
 	}
+	
+	printf("\nPlease key in the number corresponding to what you want to do\n1.View your leave\n2.Apply for leave\n3.Update leave record\n4.Exit\n");
+	scanf("%d", &num_select);	
+	
+	switch(num_select){
+		case 1:
+			//view_leave();
+			break;
+		case 2: 
+			//apply_leave();
+			break;
+		case 3:
+			//update_leave();
+			break;
+		case 4:
+			app_out(details);
+			break;
+		default:
+			printf("Please try again!");
+			break;
+	}
 }
-void app_out(struct user details){
-	log_screen(details);
+void app_out(struct user detail_repeat){
+	printf("You've successfully logged out!\n");
+	log_screen(detail_repeat);
 }
