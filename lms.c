794267b3c31adb4a5074337cb9b20f079a9ecbe8
Samarth Kapoor <samarth003@e.ntu.edu.sg>
2015-09-07 22:40:24 +0800

    comments and cleanup

--- lms.c
+++ lms.c
@@ -16,7 +16,6 @@ int *p_slapply = &sick_leave_applied;
 
 void log_screen(struct user details);
 void app_out(struct user details);
-//void view_leave(int leaves_applied, int *p_applyleaves, int sick_leave_applied, int *p_slapply);
 void view_leave(struct user details2);
 void apply_leave(struct user details1);
 void update_leave(int *p_applyleaves, int *p_slapply);
@@ -27,7 +26,7 @@ int main(){
 	log_screen(user_record);
 	return 0;
 }
-void log_screen(struct user details){
+void log_screen(struct user details){	//login authentication
 	int counter;
 	printf("user name: ");
 	scanf("%s", &details.uname);
@@ -39,14 +38,11 @@ void log_screen(struct user details){
 	}
 	options(details);	
 }
-void app_out(struct user detail_repeat){
+void app_out(struct user detail_repeat){	//log out
 	printf("You've successfully logged out!\n");
 	log_screen(detail_repeat);
 }
-void view_leave(struct user details2){
-	//sick_leave = (sick_leave - *p_slapply);	
-	//annual_leave = (annual_leave - *p_applyleaves);
-
+void view_leave(struct user details2){	
 	printf("Remaining Annual leaves: %d", annual_leave);
 	printf("\nRemaining sick leaves: %d", sick_leave);
 	options(details2);
@@ -76,7 +72,7 @@ void update_leave(int *p_applyleaves, int *p_slapply){
 }
 void options(struct user rec_user){
 	int num_select;
-	printf("\nPlease key in the number corresponding to what you want to do\n1.View your leave\n2.Apply for leave\n3.Update leave record\n4.Exit\n");
+	printf("\nPlease key in the number corresponding to what you want to do\n1.View your leave\n2.Apply for leave\n3.Exit\n");
 	scanf("%d", &num_select);	
 	
 	switch(num_select){
@@ -86,10 +82,10 @@ void options(struct user rec_user){
 		case 2: 
 			apply_leave(rec_user);
 			break;
-		case 3:
+		/*case 3:
 			update_leave(&leaves_applied, &sick_leave_applied);
-			break;
-		case 4:
+			break;*/
+		case 3:
 			app_out(rec_user);
 			break;
 		default:
