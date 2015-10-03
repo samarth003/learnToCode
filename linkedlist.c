db77c3a4372e87fa17c460894720af3e81b044d9
Samarth Kapoor <samarth003@e.ntu.edu.sg>
2015-10-03 13:55:12 +0800

    print linked list
    solved memory crash issue but linked list is traversed reversed somehow

--- linkedlist.c
+++ linkedlist.c
@@ -6,15 +6,20 @@ struct node{
 	struct node *link;
 }node_tag;
 
+#define LOOP_COUNT 6
 struct node *p_list = NULL;
 //node_tag *p_list = NULL; only works with typedef struct 
 struct node *p_pre = NULL; 
 
 struct node *insertNode(struct node *p_list, struct node *p_pre);
+void printList(struct node *p_list);
 
 int main(){
-	
-	p_list = insertNode(p_list, p_pre);	
+	int count;
+	for(count=0; count<LOOP_COUNT; count++){
+		p_list = insertNode(p_list, p_pre);
+	}	
+	printList(p_list);
 	return 0;
 }
 struct node *insertNode(struct node *p_list, struct node *p_pre){
@@ -23,17 +28,32 @@ struct node *insertNode(struct node *p_list, struct node *p_pre){
 	
 	if(p_new != NULL){
 		printf("data in the node is: ");
-		scanf("%d", p_new->data);
+		scanf("%d", &p_new->data);
+		p_new->link = NULL;
 		if(p_pre == NULL){
+		//if(p_list == NULL){
 			p_new->link = p_list;
-			p_list = p_pre = p_new;
+			//p_list = p_pre = p_new;
+			p_list = p_new;
 		}
 		else{	//insert in the middle or at the end
+			printf("inserting in the middle..");
 			p_new->link = p_pre->link;
 			p_pre->link = p_new;
 		}
 		return p_list;
 	}
 }
-
+void printList(struct node *p_list){
+	struct node *p_walk;
+	printf("Linked list is: \n");
+	p_walk = p_list;
+	
+	while(p_walk != NULL){
+		printf("%d--->", p_walk->data);
+		p_walk = p_walk->link;
+	}
+	printf("\n");
+	return;
+}
 
